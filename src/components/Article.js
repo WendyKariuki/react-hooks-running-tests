e9554cca05f31057c8f0d976cb822d02546c5e1f
WendyKariuki <phaminuswendy@gmail.com>
2024-04-22 15:20:31 +0300

    React hooks

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,7 +3,9 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      <h1>
+        please pass this test
+      </h1>
     </div>
   );
 }
